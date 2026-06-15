b70d722286d7ac4e4643c47b384d1d44d0267b73
Sri Lalitha <lalithayelisetti02@gmail.com>
2026-06-15 07:01:47 +0530

    userController.js

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,65 +1,168 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-// Register User
-const createUser = async (req, res) => {
-  try {
-    const { name, email, password, skills } = req.body;
+// Get My Profile
+const getMe = async (req, res) => {
+    try {
 
-    const existingUser = await User.findOne({ email });
+        const user = await User
+            .findById(req.user._id)
+            .select("-password");
 
-    if (existingUser) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-      skills,
-    });
-
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
-
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      skills: user.skills,
-      token,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
 };
 
-// Login User
-const loginUser = async (req, res) => {
-  res.json({
-    message: "Login API working",
-  });
+// Get Public Profile
+const getProfile = async (req, res) => {
+    try {
+
+        const user = await User
+            .findById(req.params.id)
+            .select("name skills role createdAt");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
 };
 
-// Get All Users
-const getUsers = async (req, res) => {
-  const users = await User.find({});
-  res.json(users);
+// Update Profile
+const updateProfile = async (req, res) => {
+    try {
+
+        const { name, skills , mobile} = req.body;
+
+        if (name !== undefined && name.trim() === "") {
+            return res.status(400).json({
+                message: "Name is required"
+            });
+        }
+
+        const existingUser = await User.findById(req.user._id);
+
+        if (!existingUser) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                name,
+                skills: skills ? skills : existingUser.skills,
+                 mobile: mobile ?? existingUser.mobile
+            },
+            {
+                new: true
+            }
+        ).select("-password");
+
+        res.status(200).json({
+            success: true,
+            user: updatedUser
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+// Change Password
+const changePassword = async (req, res) => {
+    try {
+
+        const { oldPassword, newPassword } = req.body;
+
+        if (!newPassword || newPassword.length < 6) {
+            return res.status(400).json({
+                message: "Password must be at least 6 characters"
+            });
+        }
+
+
+        const user = await User.findById(req.user._id);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        if (!oldPassword) {
+            return res.status(400).json({
+                message: "Old password required"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            oldPassword,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Old password incorrect"
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(
+            newPassword,
+            salt
+        );
+
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Password updated"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 };
 
 module.exports = {
-  createUser,
-  loginUser,
-  getUsers,
+    getMe,
+    getProfile,
+    updateProfile,
+    changePassword
 };

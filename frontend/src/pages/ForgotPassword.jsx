4474ab7475a5e43e57993be1fb7e54c58b723e76
udayadevi <nainalaudayadevi@gmail.com>
2026-09-07 23:32:49 +0530

    fix(frontend): resolve case-sensitive CSS import issue

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import api from "../services/api";
 import { useNavigate } from "react-router-dom";
-import "../styles/Auth.css";
+import "../styles/auth.css";
 import { toast } from "react-toastify";
 
 function ForgotPassword() {
